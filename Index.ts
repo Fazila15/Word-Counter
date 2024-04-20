a0584c658f824f427e3de1856665793e61fcb972
fazila15e <fazilamalik15@gmail.com>
2024-04-20 14:45:39 +0500

    Word Counter Done.

---
+++ Index.ts
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answers: {
+    sentence: String
+} = await inquirer.prompt(
+    {
+        name: "sentence",
+        type: "input",
+        message: `${chalk.cyan.bold("Enter your sentence to count words: ")}`
+    }
+);
+
+const words = answers.sentence.trim().split(" ");
+
+
+console.log(words);
+console.log (`${chalk.yellowBright.italic(`Your sentence has ${words.length} words.`)}`);
